d4b4de89e52c51a0b03292ec9cfbf7fd0ba97a35
lsn42 <24622966+lsn42@users.noreply.github.com>
2021-03-29 19:10:39 +0800

    move query express in ExpressApplication.java to a new controller class

---
+++ src/main/java/cn/edu/scau/express/web/controller/QueryExpressController.java
@@ -0,0 +1,53 @@
+package cn.edu.scau.express.web.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+import cn.edu.scau.express.Express;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+import javax.annotation.Resource;
+import com.google.gson.Gson;
+
+@Resource
+@RestController
+public class QueryExpressController {
+  public static final String USERNAME = "root";
+  public static final String PASSWORD = "20001005";
+  public static final String URL =
+      "jdbc:mysql://localhost:3306/mysql?useUnicode=true&characterEncoding=utf8&useSSL=false&serverTimezone=Hongkong";
+
+  public static final String EXPRESS_SQL =
+      "WITH a AS(select ex_id, customer.express_list.create_time , customer.transport_list.start_time , customer.transport_list.end_time , customer.transport_list.truck_id , customer.transport_list.plane_id ,customer.transport_list.postman_id from customer.express_list inner join customer.transport_list on customer.express_list.trans_id = customer.transport_list.trans_id where customer.transport_list.start_time > customer.express_list.create_time),b AS(SELECT ex_id, customer.express_list.create_time, customer.stored_list.intime, customer.stored_list.outtime, customer.stored_list.stored_id FROM customer.express_list INNER JOIN customer.stored_list ON customer.express_list.store_id = customer.stored_list.stored_id where customer.stored_list.intime > customer.express_list.create_time)SELECT * FROM a NATURAL JOIN b WHERE a.ex_id=";
+
+  @GetMapping(value = "/query/express/{id}")
+  public String queryExpress(@PathVariable("id") String id) {
+    Express e = new Express(id);
+    try {
+      Connection con;
+      con = DriverManager.getConnection(URL, USERNAME, PASSWORD);
+      Statement statement = con.createStatement();
+      String sql = EXPRESS_SQL + id + ";";
+      ResultSet rs = statement.executeQuery(sql);
+      rs.next();
+
+      e.truckId = "" + rs.getInt("truck_id");
+      e.planeId = "" + rs.getInt("plane_id");
+      e.postmanId = "" + rs.getInt("postman_id");
+      e.storeId = "" + rs.getInt("stored_id");
+      e.createTime = rs.getString("create_time");
+      e.startTime = rs.getDate("start_time").toString();
+      e.endTime = rs.getDate("end_time").toString();
+      e.inTime = rs.getString("intime");
+      e.outTime = rs.getString("outtime");
+
+      con.close();
+
+    } catch (Exception ex) {
+      ex.printStackTrace();
+    }
+    return (new Gson()).toJson(e);
+  }
+}
