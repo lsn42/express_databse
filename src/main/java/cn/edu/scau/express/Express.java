44a27f7d1e42f088d4f274298a6fdee0c31adb21
lsn42 <24622966+lsn42@users.noreply.github.com>
2021-03-29 19:43:27 +0800

    modify query express return json format

--- src/main/java/cn/edu/scau/express/Express.java
+++ src/main/java/cn/edu/scau/express/Express.java
@@ -2,26 +2,24 @@ package cn.edu.scau.express;
 
 public class Express {
   public String id;
-  public String truckId;
-  public String planeId;
-  public String postmanId;
-  public String storeId;
   public String createTime;
-  public String startTime;
-  public String endTime;
-  public String inTime;
-  public String outTime;
+  public String postmanId;
+  public Route firstRoute;
+  public Route secondRoute;
 
   public Express(String id) {
     this.id = id;
-    this.truckId = null;
-    this.planeId = null;
-    this.postmanId = null;
-    this.storeId = null;
-    this.createTime = null;
-    this.startTime = null;
-    this.endTime = null;
-    this.inTime = null;
-    this.outTime = null;
+    this.firstRoute = new Route();
+    this.secondRoute = new Route();
+  }
+
+  public class Route {
+    public int type;
+    public String id;
+    public String startTime;
+    public String endTime;
+
+    public Route() {
+    };
   }
 }
--- src/main/java/cn/edu/scau/express/web/controller/QueryExpressController.java
+++ src/main/java/cn/edu/scau/express/web/controller/QueryExpressController.java
@@ -1,17 +1,16 @@
 package cn.edu.scau.express.web.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
-import cn.edu.scau.express.Express;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.Statement;
-import javax.annotation.Resource;
 import com.google.gson.Gson;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+import cn.edu.scau.express.Express;
 
-@Resource
 @RestController
 public class QueryExpressController {
   public static final String USERNAME = "root";
@@ -22,6 +21,7 @@ public class QueryExpressController {
   public static final String EXPRESS_SQL =
       "WITH a AS(select ex_id, customer.express_list.create_time , customer.transport_list.start_time , customer.transport_list.end_time , customer.transport_list.truck_id , customer.transport_list.plane_id ,customer.transport_list.postman_id from customer.express_list inner join customer.transport_list on customer.express_list.trans_id = customer.transport_list.trans_id where customer.transport_list.start_time > customer.express_list.create_time),b AS(SELECT ex_id, customer.express_list.create_time, customer.stored_list.intime, customer.stored_list.outtime, customer.stored_list.stored_id FROM customer.express_list INNER JOIN customer.stored_list ON customer.express_list.store_id = customer.stored_list.stored_id where customer.stored_list.intime > customer.express_list.create_time)SELECT * FROM a NATURAL JOIN b WHERE a.ex_id=";
 
+  @ResponseBody
   @GetMapping(value = "/query/express/{id}")
   public String queryExpress(@PathVariable("id") String id) {
     Express e = new Express(id);
@@ -33,15 +33,25 @@ public class QueryExpressController {
       ResultSet rs = statement.executeQuery(sql);
       rs.next();
 
-      e.truckId = "" + rs.getInt("truck_id");
-      e.planeId = "" + rs.getInt("plane_id");
-      e.postmanId = "" + rs.getInt("postman_id");
-      e.storeId = "" + rs.getInt("stored_id");
       e.createTime = rs.getString("create_time");
-      e.startTime = rs.getDate("start_time").toString();
-      e.endTime = rs.getDate("end_time").toString();
-      e.inTime = rs.getString("intime");
-      e.outTime = rs.getString("outtime");
+      e.postmanId = "" + rs.getInt("postman_id");
+
+      // first route
+      String t = "" + rs.getInt("plane_id");
+      e.firstRoute.type = 0;
+      if (rs.wasNull()) {
+        t = "" + rs.getInt("truck_id");
+        e.firstRoute.type = 1;
+      }
+      e.firstRoute.id = t;
+      e.firstRoute.startTime = rs.getDate("start_time").toString();
+      e.firstRoute.endTime = rs.getDate("end_time").toString();
+
+      // second route
+      e.secondRoute.type = 2;
+      e.secondRoute.id = "" + rs.getInt("stored_id");
+      e.secondRoute.startTime = rs.getString("intime");
+      e.secondRoute.endTime = rs.getString("outtime");
 
       con.close();
 
