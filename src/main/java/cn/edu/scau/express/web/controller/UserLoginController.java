5450629e4ce85ce854d47f0f2ad97350c96ab3f3
handsomelys <m13502254993@163.com>
2021-04-20 21:57:25 +0800

    user login and register

--- src/main/java/cn/edu/scau/express/web/controller/UserLoginController.java
+++ src/main/java/cn/edu/scau/express/web/controller/UserLoginController.java
@@ -5,21 +5,26 @@ import cn.edu.scau.express.service.UserLoginServicesImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.sql.SQLIntegrityConstraintViolationException;
 import java.util.List;
 
-@Controller
-
+@RestController
+@CrossOrigin
 public class UserLoginController {
     @Autowired
     UserLoginServicesImpl userLoginServicesImpl;
 
-    @RequestMapping("/toLogin")
+    @PostMapping("/toLogin")
     public String toLogin() {
         return "login_test";
     }
 
-    @RequestMapping("/LoginSuccess")
+    @PostMapping("/LoginSuccess")
     public String LoginSuccess(Model model, UserLogin userLogin) {
         UserLogin userLogin1 = userLoginServicesImpl
                 .queryById(userLogin.getUser_id(), userLogin.getUser_psw());
@@ -34,24 +39,37 @@ public class UserLoginController {
                     userLoginServicesImpl.queryPswById(userLogin1.getUser_id());
             System.out.println(userLogin1.getUser_psw());
             System.out.println(psw);
-            return "success";
-        } else {
-            model.addAttribute("data", "该用户不存在，或密码错误");
-            return "login_test";
+            if(userLogin1.getUser_permission().equals("admin")){
+                return "admin";
+            }
+            else if (userLogin1.getUser_permission().equals("customer")){
+                return "customer";
+            }
         }
+        else{
+            model.addAttribute("data", "该用户不存在，或密码错误");
+
+        }
+        return "login failed";
     }
 
-    @RequestMapping("/toRegister")
+    @PostMapping("/toRegister")
     public String toRegister() {
         return "register";
     }
 
-    @RequestMapping("/RegisterSuccess")
-    public String toRegisterSuccess(Model model, UserLogin userLogin) {
-        userLoginServicesImpl.add(userLogin);
-        System.out.println("插入成功");
-        model.addAttribute("data", "注册成功，请登录！");
-        return "login_test";
+    @PostMapping("/RegisterSuccess")
+    public String toRegisterSuccess(Model model, UserLogin userLogin) throws SQLIntegrityConstraintViolationException {
+        try{
+            userLoginServicesImpl.add(userLogin);
+            System.out.println("插入成功");
+            model.addAttribute("data", "注册成功，请登录！");
+            return "true";
+        } catch (Exception e){
+            e.printStackTrace();
+            model.addAttribute("data","已有该用户存在");
+        }
+            return "false";
     }
 
 }
