737267c7adb692bb0f936996edc8fbefdad9b9c2
handsomelys <m13502254993@163.com>
2021-04-21 16:30:48 +0800

    drop some duplicated files

---
+++ database/define/user.sql
@@ -0,0 +1,8 @@
+drop table if exists user_group;
+CREATE TABLE `user_group` (
+                              `user_id` varchar(64) NOT NULL,
+                              `user_name` varchar(64) DEFAULT NULL,
+                              `user_psw` varchar(64) DEFAULT NULL,
+                              `user_permission` char(16) DEFAULT NULL,
+                              PRIMARY KEY (`user_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
